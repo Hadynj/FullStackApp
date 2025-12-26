68d1ea97d7cfdbb58de9b8d6492cdf89f2ef71de
hadynj <hadynjiee@gmail.com>
2025-12-26 01:28:48 -0500

    Initial commit

--- PERN-API/index.js
+++ PERN-API/index.js
@@ -2,10 +2,20 @@ const express = require('express')
 const app = express ()
 const PORT = 5500
 
+// Routes
 app.get('/', (req, res) =>{
     // we'll do some stuff here
     res.send("Hellooooo from the server!")
 })
+
+// CRUD
+// CREATE
+// READ
+// UPDATE
+// DELETE
+
+
+
 
 // Starting Express on our PORAT
 app.listen(PORT, () => {
