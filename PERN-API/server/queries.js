2f630fcdbeee857257186832f7c17b8a774847f9
hadynj <hadynjiee@gmail.com>
2025-12-26 04:00:25 -0500

    Initial commit

--- PERN-API/server/queries.js
+++ PERN-API/server/queries.js
@@ -17,17 +17,19 @@ const pool = new POOL({
 const getLinks = () => {
     pool.query('SELECT * FROM links ORDER BY id ASC', (error, result) =>{
         if(error){
-            throw error;
+            throw error
         }
         result.status(200).json(result.rows)
     })
 }
+
 
     // update link in the db
 
     // delete link in the db
 
     module.exports = {
+        getLinks,
 
     }
 
